5d4aac71fa9dcde36ce7105151bb166dba60e275
Robin Hung <robin840615@gmail.com>
2018-11-01 16:16:02 -0400

    README instructions

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # PathCount
+
+In this project, there will be two Docker containers, one for Flask Server and one for PostgreSQL database, communicating with each other. The Server can accept GET requests, and update that GET request path into the Postgres database, then returning all the existing request paths and how many time those paths have been requested.
+
+
+![demo](https://preview.ibb.co/d6DR50/Screen-Shot-2018-11-01-at-4-11-04-PM.png)
+
+## Instructions
+
+```bash
+# List current network default
+$ docker network ls
+
+# Create new docker network
+$ docker network create <NAME>
+```
+
+### PostgreSQL
+
+Be sure to use `cd postgres` to  navigate to the postgres directory!
+
+```bash
+$ docker run --network pgnet --name pg-db -e POSTGRES_PASSWORD=password -d postgres
+
+# Lookup that your container IP
+$ docker container inspect mypg -f '{{.NetworkSettings.Networks.pgnet.IPAddress}}'
+172.20.0.2
+
+# Initialize the database
+$ docker run -i --network pgnet -e PGPASSWORD=password postgres psql -h <IP_ADDRESS> -U postgres < init.sql
+
+# Postgres console
+$ docker exec -it pg-db psql -U postgres
+
+postgres=# \dt
+
+postgres=# SELECT * FROM pathcount;
+```
+
+### Flask
+
+`cd flask-server` first!
+
+```bash
+# Build the docker image
+$ docker build pathcount-serve .
+
+$ docker run --network pgnet --name pathcount -p 9090:8080 pathcount-server
+```
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,40 +1,7 @@
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@171.20.0.2:5432/pathcount'
-# db = SQLAlchemy(app)
-
-# def connect_to_DB():
-#     connectionString = 'dbname=pg-db user=postgres password=password host=172.20.0.2'
-#     try:
-#         psycopg2.connect("dbname='postgres' user='postgres' host='172.20.0.2' password='password'")
-#     except:
-#         print("Can't connect to database")
-
-# if db is None:
-#     @app.route('/')
-#     def index():
-#         return "DB not found!"
-# else:
-#     class URLPath(db.Model):
-#         __tablename__ = "url_path_count"
-#         url_path = db.Column('url_path', db.String())
-#         path_count = db.Column('path_count', db.Integer)
-#
-#     # @app.route('/'):
-#
-#
-#     @app.route('<path>'):
-#     def queryDB(path):
-#         # Exists
-#         if URLPath.query.filterby(url_path = path) != None:
-#             URLPath.path_count += 1
-#         # Not Exists, Create new row!
-#         else:
-
-
 
 # Catch all requests
 @app.route('/', defaults={'path': ''})
@@ -49,7 +16,7 @@ def catch_all(path):
     curr.execute("INSERT INTO pathcount (path, count) VALUES (%s, 1) ON CONFLICT (path) DO UPDATE SET count = pathcount.count + 1 RETURNING count;", (path,))
     conn.commit()
 
-    curr.execute("SELECT path, count from pathcount ORDER BY count DESC;")
+    curr.execute("SELECT path, count from pathcount ORDER BY path ASC;")
     rows = curr.fetchall()
     print("\nShow me the databases:\n")
 
