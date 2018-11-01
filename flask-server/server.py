0c77f63fe7acef654c224de6901ccd7ed3d583c5
Robin Hung <robin840615@gmail.com>
2018-11-01 17:23:21 -0400

    compose

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import psycopg2
+import os
 
 app = Flask(__name__)
 
@@ -7,8 +8,17 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
+    POSTGRES = {
+        'user': os.environ['POSTGRES_USER'],
+        'pw': os.environ['POSTGRES_PASSWORD'],
+        'db': os.environ['POSTGRES_DATABASE'],
+        'host': os.environ['POSTGRES_HOST'],
+        'port': os.environ['POSTGRES_PORT'],
+    }
+
     try:
-        conn = psycopg2.connect("dbname='postgres' user='postgres' host='172.20.0.2' password='password'")
+        conn = psycopg2.connect("dbname=%(db)s user=%(user)s password=%(pw)s host=%(host)s port=%(port)s" % POSTGRES)
+        # conn = psycopg2.connect("dbname='postgres' user='postgres' host='172.20.0.2' password='password'")
     except:
         print("Can't connect to database!")
 
@@ -18,7 +28,6 @@ def catch_all(path):
 
     curr.execute("SELECT path, count from pathcount ORDER BY path ASC;")
     rows = curr.fetchall()
-    print("\nShow me the databases:\n")
 
     pathcount_result = ''
     for row in rows:
